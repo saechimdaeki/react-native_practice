9ab44b4bcd92fdcb9c515781bd8cee7458a6b1ed
saechimdaeki <anima94@naver.com>
2019-10-30 22:10:14 +0900

    set image res

--- second/App.js
+++ second/App.js
@@ -1,24 +1,11 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Image } from 'react-native';
 
-class Greeting extends Component {
+export default class Bananas extends Component {
   render() {
-    return (
-      <View style={{ alignItems: 'center' }}>
-        <Text>Hello {this.props.name}!</Text>
-      </View>
-    );
-  }
-}
-
-export default class LotsOfGreetings extends Component {
-  render() {
-    return (
-      <View style={{ alignItems: 'center', top: 50 }}>
-        <Greeting name="Rexxar" />
-        <Greeting name="Jaina" />
-        <Greeting name="Valeera" />
-      </View>
-    );
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg',
+    };
+    return <Image source={pic} style={{ width: 193, height: 110 }} />;
   }
 }
