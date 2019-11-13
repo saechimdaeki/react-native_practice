221ce9e8c134160b43127d38dd04079e5a4a9e80
saechimdaeki <anima94@naver.com>
2019-11-13 22:52:41 +0900

    stylecomponent #4

--- kawaiproject/App.js
+++ kawaiproject/App.js
@@ -1,18 +1,37 @@
 import React from 'react';
-import {TextInput, StyleSheet, Text, View,StatusBar, Dimensions , Platform} from 'react-native';
+import {TextInput, StyleSheet, Text, View,StatusBar, Dimensions , Platform ,ScrollView} from 'react-native';
+import ToDo from "./ToDo";
 const { height, width} = Dimensions.get("window");
 export default class App extends React.Component {
+  state={
+    newToDo: ""
+  }
   render(){
+    const {newToDo}=this.state;
   return (
     <View style={styles.container}>
       <StatusBar barStyle="light-content"/>
       <Text style={styles.title}>Kawai TODO </Text>
       <View style={styles.card}>
-        <TextInput style={styles.input} placeholder={"new to do"}/>
+        <TextInput style={styles.input} placeholder={"new to do"}
+         value={newToDo} 
+         onChangeText={this._controlNewTodo}
+         placeholderTextColor={"#999"}
+         returnKeyType={"done"}
+         autoCorrect={false}
+         />
+         <ScrollView>
+          <ToDo/>
+         </ScrollView>
       </View>
     </View>
   );
 }
+_controlNewTodo=text=>{
+  this.setState({
+newToDo: text
+  });
+};
 }
 const styles = StyleSheet.create({
   container: {
@@ -48,5 +67,11 @@ const styles = StyleSheet.create({
         elevation:3
       }
     })
+  },
+  input: {
+    padding:20,
+    borderBottomColor:"#bbb",
+    borderBottomWidth: 1,
+    fontSize: 25 
   }
 });
---
+++ kawaiproject/ToDo.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import {View, Text, TouchableOpacity, StyleSheet} from "react-native"
+export default class ToDo extends Component{
+    render(){
+        return(
+        <View>
+            <Text>hello todo js </Text>
+        </View>
+        );
+    }
+}
+const styles= StyleSheet.create({});
