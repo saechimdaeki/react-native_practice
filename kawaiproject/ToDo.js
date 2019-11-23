02fe7bc58c1eeff600d3f3533d1d613284bf2dfb
saechimdaeki <anima94@naver.com>
2019-11-23 14:31:34 +0900

    styling the component

--- kawaiproject/ToDo.js
+++ kawaiproject/ToDo.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import {View, Text, TouchableOpacity, StyleSheet, Dimensions} from "react-native"
+
 const {width , height} = Dimensions.get("window");
 export default class ToDo extends Component{
     state= {
@@ -7,31 +8,67 @@ export default class ToDo extends Component{
         isCompleted: false
     }
     render(){
-        const {isCompleted} = this.state;
+        const {isCompleted, isEditing} = this.state;
         return(
             <View style={styles.container}>
+                <View style={styles.column}>
             <TouchableOpacity onPress={this._toggleComplete}>
                 <View style={[styles.circle, isCompleted ? styles.completedCircle: styles.uncompletedCircle]}/>
             </TouchableOpacity>
-            <Text style={styles.text}>hello todo js </Text>
+            <Text style={[styles.text, isCompleted ? styles.completedText: styles.uncompletedText]}>hello todo js </Text>
         </View>
+      
+         {isEditing ? 
+             (<View style={styles.actions}>
+                <TouchableOpacity onPressOut={this._finishEditing}>
+                    <View style={styles.actionContainer}>
+                        <Text style={styles.actionText}>😀</Text>
+                    </View>
+                </TouchableOpacity>
+             </View>
+          ):  (<View style={styles.actions}>
+         <TouchableOpacity onPressOut={this._startEditing}>
+             <View style={styles.actionContainer}>
+                 <Text style={styles.actionText}>✏️</Text>
+             </View>
+         </TouchableOpacity>
+         <TouchableOpacity>
+             <View style={styles.actionContainer}>
+                 <Text style={styles.actionText}>❌</Text>
+             </View>
+         </TouchableOpacity>
+          </View>)}
+         </View>
+        
         );
     }
     _toggleComplete= () => {
         this.setState(prevState =>{
             return{
                 isCompleted : !prevState.isCompleted
-            }
-        })
-    }
-}
+            };
+        });
+    };
+    _startEditing = () => {
+        this.setState({
+            isEditing:true
+        });
+    };
+    _finishEditing = () =>{
+        this.setState({
+            isEditing: false
+        });
+    };
+    
+} 
 const styles= StyleSheet.create({
 container: {
     width: width - 50 ,
     borderBottomColor:"#bbb",
     borderBottomWidth:StyleSheet.hairlineWidth,
     flexDirection:"row",
-    alignItems: "center"
+    alignItems: "center",
+    justifyContent:"space-between"
 },
 circle:{
 width:30,
@@ -52,6 +89,25 @@ text:{
     fontWeight:"600",
     fontSize:20,
     marginVertical:20
+},completedText : {
+    color:"#bbb",
+    textDecorationLine:"line-through"
+},
+uncompletedText:{
+    color:"#353839"
+},
+column:{
+    flexDirection:"row",
+    alignItems :"center",
+    width:width /2,
+    justifyContent:"space-between"
+},
+actions:{
+    flexDirection:"row"
+},
+actionContainer:{
+    marginVertical: 10,
+    marginHorizontal: 10
 }
 
 });
