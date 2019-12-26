5f7103e8d057d1e67956b3270646c635975480a0
saechimdaeki <anima94@naver.com>
2019-12-26 17:39:52 +0900

    날씨어플 현재 console에 정보나오기까지만 완료

--- wheatherApp/App.js
+++ wheatherApp/App.js
@@ -1,19 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from "./Loading";
+import * as Location from "expo-location";
+import { Alert } from 'react-native';
+import axios from "axios";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>helllooollllooo!</Text>
-    </View>
-  );
+const API_KEY="637ece8e9acd7936fbcef19ee8215dd5";
+export default class extends React.Component {
+  state={
+    isLoading: true
+  };
+  getWeather=async(latitude, longitude) => {
+    const {data} = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
+    );
+    console.log(data);
+  };
+  getLocation= async () => {
+    try{
+      await Location.requestPermissionsAsync();
+      const {
+        coords: {latitude, longitude }
+      } = await Location.getCurrentPositionAsync();
+      //send to api 
+     // console.log(coords.latitude, coords.longitude);
+      this.getWeather(latitude,longitude);
+      this.setState({ isLoading : false });
+    }catch(error){
+      Alert.alert("can't find you.","so sad");
+    }
+  };
+  componentDidMount(){
+    this.getLocation();
+  }
+  render(){
+    const {isLoading}=this.state;
+    return isLoading? <Loading/> :null;
+  }
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
