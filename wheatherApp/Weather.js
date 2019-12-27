7681e4542dc581bd68c74881ca99e7503f23fd84
saechimdaeki <anima94@naver.com>
2019-12-27 14:49:44 +0900

    change method

--- wheatherApp/Weather.js
+++ wheatherApp/Weather.js
@@ -11,7 +11,8 @@ export default function Weather({ temp }) {
 }
 
 Weather.propTypes = {
-  temp: PropTypes.number.isRequired
+  temp: PropTypes.number.isRequired,
+  condition: PropTypes.oneOf(["ThunderStorm","Drizzle","Snow","Atomsphere","Clear","clouds","Haze","Mist","Dust"]).isRequired
 };
 
 const styles = StyleSheet.create({
