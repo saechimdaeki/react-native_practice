37eb11c11786ae3cc80331f2dba10fa52c551ff7
saechimdaeki <anima94@naver.com>
2019-12-30 17:25:51 +0900

    생각처럼 되지않는 언어공부

--- awesome2/App.js
+++ awesome2/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { Button,Text, View,TextInput, Image,StyleSheet} from 'react-native';
-import CheckBox from '@react-native-community/checkbox';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+
 
 
 const styles=StyleSheet.create({
   abc:{
-
   },
   for_login:{
       flex:1,
@@ -15,10 +16,14 @@ const styles=StyleSheet.create({
 
 
 
-export default class dimension extends Component{
+class dimension extends Component{
+  
+    
   _onPressButton(){
     alert('tapped the button')
+    
   }
+  
   render(){
     return (
         <View style={styles.Container}>
@@ -36,23 +41,46 @@ export default class dimension extends Component{
 
         <View style={{marginTop:10}}>
           <Button
-          
-            onPress={this._onPressButton}
+           onPress={()=>this.props.navigation.navigate('Details')}
             title="로그인"
             color="blue"
-          />
+             />
+
+         
          
         </View>
-         <View style={{flex:1}}>
-           <Text>dada13131313</Text>
-           
+         <View style={{marginTop:20,flex:1, flexDirection:'row' ,dimension:1}}>
+         <Image source={require('./image/android.png')}
+       style={{width:'40%' ,height: 100}} />
+       <Image source={require('./image/iphone.png')}
+       style={{marginLeft:40,width:'40%' ,height: 100}} />
+          </View>
         </View>
-        
-        </View>
-       
-       
-       
-  
     )
   }
 }
+class DetailsScreen extends React.Component {
+  render() {
+  return (
+  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+  <Text>Seonoh Detail Screen</Text>
+  <Button
+  title = 'Go Home screen'
+  onPress = {()=>this.props.navigation.navigate('dim')}/>
+  </View>
+  );
+  }
+  }
+  
+
+const AppNavigator = createStackNavigator(
+  {
+    dim:dimension,
+    Details:DetailsScreen
+  },
+{
+  initialRouteName:'dim',
+}
+);
+
+export default createAppContainer(AppNavigator);
