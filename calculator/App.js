2525e56f30eb7e284c7fb6f2bf132ccc30bfc5c6
saechimdaeki <anima94@naver.com>
2020-01-06 14:18:16 +0900

    calculator re-init and finish the layout positon

---
+++ calculator/App.js
@@ -0,0 +1,110 @@
+import React, {Component } from 'react';
+import {StyleSheet, Text,View, Button } from 'react-native';
+
+export default class App extends Component {
+  constructor() {
+    super()
+    this.state={}
+  }
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.result}>
+            <Text style={styles.resultText}>11*11</Text>
+        </View>
+        <View style={styles.calculation}>
+          <Text style={styles.calculationText}>121</Text>  
+        </View> 
+        <View style={styles.buttons}>
+          <View style={styles.numbers}>
+            <View style={styles.rows}>
+              <Button title="0"/>
+              <Button title="0"/>
+              <Button title="0"/>
+
+            </View>
+            <View style={styles.rows}>
+              <Button title="0"/>
+              <Button title="0"/>
+              <Button title="0"/>
+
+            </View>
+            <View style={styles.rows}>
+              <Button title="0"/>
+              <Button title="0"/>
+              <Button title="0"/>
+
+            </View>
+            <View style={styles.rows}>
+              <Button title="0"/>
+              <Button title="0"/>
+              <Button title="0"/>
+
+            </View>
+
+          </View>
+          <View style={styles.operation}>
+              <Button title="+"/>
+              <Button title="+"/>
+              <Button title="+"/>
+              <Button title="+"/>
+              </View>
+        </View>    
+      </View>
+      
+    );
+  } 
+}
+
+const styles=StyleSheet.create({
+  container:{
+    flex:1,
+  },
+  resultText:{
+    fontSize:30,
+    color:'white',
+
+  },
+  calculationText:{
+    fontSize:24,
+    color:'white',
+  },
+  rows:{
+    flexDirection:'row',
+    flex:1,
+    justifyContent:"space-around",
+    alignItems:'center',
+  },
+  result:{
+    flex:2,
+    backgroundColor:'red',
+    justifyContent:'center',
+    alignItems:'flex-end'
+  },
+  calculation:{
+    flex:1,
+    backgroundColor:'green',
+    justifyContent:'center',
+    alignItems:'flex-end'
+  },
+  buttons:{
+    flex:7,
+    flexDirection:'row',
+  },
+  numbers:{
+    flex:3,
+    backgroundColor:'yellow',
+  },
+  operation:{
+    flex:1,
+    justifyContent:'space-around',
+    alignItems:'stretch',
+    backgroundColor:'black'
+  },
+  row:{
+
+  },
+
+
+})
